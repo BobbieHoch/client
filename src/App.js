c348800678c8691d9708ffbf47fd8bda1d0e1f0d
BobbieHoch <rwhochstadt@gmail.com>
2023-05-25 17:49:52 -0400

    updated upon completion

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Profile from "./components/Profile";
 import BookUser from "./components/BookUser"; 
